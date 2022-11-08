7d23a84196e9f210e9423cd04f556061dbc200d0
shahryarkiani <shahryarkiani04@gmail.com>
2022-11-08 12:00:44 -0500

    Refactor LibrarySystem/views.py to move card number generation and email send logic to separate utilfuncs.py

---
+++ LibrarianSystem/utilfuncs.py
@@ -0,0 +1,33 @@
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.template.loader import get_template
+from django.core.mail import send_mail
+from django.contrib.auth.tokens import default_token_generator
+from random import randint
+from django.contrib.auth.models import User
+
+
+def generateCardNumber():
+    rand = randint(1_000_000_00, 9_999_999_99)
+    while(User.objects.filter(pk=str(rand)).exists()):
+        rand = randint(1_000_000_00, 9_999_999_99)
+    return str(rand)
+
+def sendCreatePasswordEmail(user):
+    subject = "Setup Your Library Card Password"
+    toMail = user.email
+    context = {
+		'domain':'127.0.0.1:8000',
+		'uidb64': urlsafe_base64_encode(force_bytes(user.pk)),
+		'user': user,
+		'token': default_token_generator.make_token(user),
+		'protocol': 'http',
+	}
+    message = get_template('register_email.html').render(context)
+    
+    send_mail(
+        subject,
+        message,
+        from_email='Test@mail.com',
+        recipient_list=[toMail]
+    )
--- LibrarianSystem/views.py
+++ LibrarianSystem/views.py
@@ -1,15 +1,10 @@
-from django.utils.http import urlsafe_base64_encode
-from django.utils.encoding import force_bytes
-from random import randint
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from LibraryCatalog.forms import searchForm
 from .forms import signupForm
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.decorators import login_required
-from django.template.loader import get_template
-from django.core.mail import send_mail
-from django.contrib.auth.tokens import default_token_generator
+from .utilfuncs import generateCardNumber, sendCreatePasswordEmail
 
 # Create your views here.
 
@@ -47,27 +42,3 @@ def manageRegister(request):
         }
         return render(request, 'manageRegister.html', context=context)
     
-def generateCardNumber():
-    rand = randint(1_000_000_00, 9_999_999_99)
-    while(User.objects.filter(pk=str(rand)).exists()):
-        rand = randint(1_000_000_00, 9_999_999_99)
-    return str(rand)
-
-def sendCreatePasswordEmail(user):
-    subject = "Setup Your Library Card Password"
-    toMail = user.email
-    context = {
-		'domain':'127.0.0.1:8000',
-		'uidb64': urlsafe_base64_encode(force_bytes(user.pk)),
-		'user': user,
-		'token': default_token_generator.make_token(user),
-		'protocol': 'http',
-	}
-    message = get_template('register_email.html').render(context)
-    
-    send_mail(
-        subject,
-        message,
-        from_email='Test@mail.com',
-        recipient_list=[toMail]
-    )
